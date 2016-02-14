ef988cbb26a8006da6c53818bda7f6adf5214d99
mersocarlin <mersocarlin@gmail.com>
2016-02-14 12:16:29 -0200

    Fix Loader animation

--- app/components/loader.js
+++ app/components/loader.js
@@ -6,7 +6,7 @@ export default React.createClass({
   render () {
     return (
       <div className="loader-component">
-        <i className="icon icon-pokeball fa-spin">
+        <i className="icon icon-pokeball fa fa-spin">
         </i>
       </div>
     );
